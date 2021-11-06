1895f184b70cb698061b99fcc16ae8b8436da4b9
srinadh1304 <srinadh1304@gmail.com>
2021-11-06 08:06:33 +0530

    ADDED: dto files to return json object

--- src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
@@ -1,58 +1,24 @@
 package com.bridgelabz.addressbook.dto;
-
-
 public class AddressBookDTO {
-	private int id;
-    private String name;
-    private String address;
-    private String phoneNo;
-    private String email;
-    
+	
+    public String name;
+    public String address;
+    public String phoneNo;
+    public String email;
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
+	public AddressBookDTO(String name, String address, String phoneNo, String email) {
+		super();
 		this.name = name;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
 		this.address = address;
-	}
-
-	public String getPhoneNo() {
-		return phoneNo;
-	}
-
-	public void setPhoneNo(String phoneNo) {
 		this.phoneNo = phoneNo;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
 		this.email = email;
 	}
 
 	@Override
 	public String toString() {
-		return "AddressBookDTO [id=" + id + ", name=" + name + ", address=" + address + ", phoneNo=" + phoneNo
-				+ ", email=" + email + "]";
+		return "AddressBookDTO [name=" + name + ", address=" + address + ", phoneNo=" + phoneNo + ", email=" + email
+				+ "]";
 	}
 
+	
 }
