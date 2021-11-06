e7af86935f2eac1523239a553b1c9b9dea5197b7
srinadh1304 <srinadh1304@gmail.com>
2021-11-06 08:27:56 +0530

    ADDED: logging using slf4j

--- src/main/java/com/bridgelabz/addressbook/AddressbookApplication.java
+++ src/main/java/com/bridgelabz/addressbook/AddressbookApplication.java
@@ -2,12 +2,15 @@ package com.bridgelabz.addressbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class AddressbookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressbookApplication.class, args);
+		log.info("Addrress book app started");
 	}
 
 }
