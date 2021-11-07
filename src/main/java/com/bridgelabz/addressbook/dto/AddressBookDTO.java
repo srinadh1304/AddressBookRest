a12317f9556c31962d68c5d64d76a7b6fb61755d
srinadh1304 <srinadh1304@gmail.com>
2021-11-07 10:47:30 +0530

    ADDED: validations for the address fields

--- src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
@@ -1,9 +1,20 @@
 package com.bridgelabz.addressbook.dto;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 public class AddressBookDTO {
 	
+	@NotEmpty(message="Name cannot be null")
+	@Pattern(regexp = "^[A-Z|a-z]{2,}$",message ="Name Invalid")
     public String name;
+	
+	@Pattern(regexp = "^[A-Z|a-z]{3,}$",message ="Address Invalid")
     public String address;
+    
+    @Pattern(regexp = "^(\\+\\d{2}|\\d{2})?\\d{10}$",message ="Phone number Invalid")
     public String phoneNo;
+    
+    @Pattern(regexp = "^[a-zA-Z]*[@][.a-zA-z]*$",message ="Email Invalid")
     public String email;
 
 	public AddressBookDTO(String name, String address, String phoneNo, String email) {
